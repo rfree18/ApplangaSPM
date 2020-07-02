72ed1da580374ca3a15d04c3c50e914c43cca928
Ross Freeman <ross.freeman@ww.com>
2020-07-02 09:35:44 -0400

    Simplify Package.swift file

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "applangaSPM",
+    name: "Applanga",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "applangaSPM",
-            targets: ["applangaSPM"]),
+            name: "Applanga",
+            targets: ["Applanga"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,12 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "applangaSPM",
-            dependencies: ["Applanga"]),
-        .testTarget(
-            name: "applangaSPMTests",
-            dependencies: ["applangaSPM"]),
         .binaryTarget(name: "Applanga", path: "Libs/ApplangaIOS.xcframework"
               ),
     ]
